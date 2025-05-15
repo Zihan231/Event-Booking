9fbc09428d1b738052f52482a322f096d5a2a3c7
Antu-Devnath <antu3841@gmail.com>
2025-05-15 23:33:17 +0600

    antu

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -1,4 +1,20 @@
 <?php
+session_start();
+$_SESSION['status'] = false;
 
- echo "This is the page ";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    if ($email == "" || $password == "") {
+        $_SESSION["status"] = true;
+        header('Location: ../View/login.html');
+    } else if ($email == $password) {
+        header('Location: ../View/DashBoard.html');
+    } else {
+        echo "Invalid user..!";
+    }
+} else {
+    header('Location: ../View/login.html');
+}
 ?>
